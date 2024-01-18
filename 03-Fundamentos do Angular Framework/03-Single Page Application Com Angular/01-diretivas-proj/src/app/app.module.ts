81a7e29d4fdef5610dbdcd7a920d49b0a378ac9a
Bruno Silva <68924002+BrunoRafaSilva@users.noreply.github.com>
2024-01-17 21:20:35 -0300

    avanço no bootcamp
    Compreendendo a utilização de switch case e a concentração de componente em um module, economizar linhas de deixar o APP mais centralizado

--- 03-Fundamentos do Angular Framework/03-Single Page Application Com Angular/01-diretivas-proj/src/app/app.module.ts
+++ 03-Fundamentos do Angular Framework/03-Single Page Application Com Angular/01-diretivas-proj/src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { ListModule } from './list/list.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { CompAtributosComponent } from './comp-atributos/comp-atributos.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CardComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CardComponent, CompAtributosComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ListModule],
+  exports: [],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
